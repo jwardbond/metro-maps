13890a1de2feab8362c0d2bbff622aa9f16673e0
Jesse Ward-Bond <jessewb.c@gmail.com>
2022-03-18 12:17:35 -0400

    Added a method to determine octilinear segment for each edge

--- create_graph.py
+++ create_graph.py
@@ -1,4 +1,6 @@
 import json
+import math
+from types import SimpleNamespace
 
 
 class Node:
@@ -15,8 +17,8 @@ class Edge:
         self.relation = edge_as_dict['relation']
         self.time = edge_as_dict['metadata']['time']
         self.line = edge_as_dict['metadata']['lines']
-        self.source_direction = []
-        self.target_direction = []
+        self.source_directions = []
+        self.target_directions = []
 
 
 class Graph: 
@@ -29,8 +31,44 @@ class Graph:
         self.nodes = {node['id']: Node(node) for node in x['nodes']}
         self.edges = {i: Edge(edge) for i, edge in enumerate(x['edges'])}
 
+    def calc_sections(self, sections=[0,1,2,3,4,5,6,7]):
+        '''
+        Determines the "sections" of all edges in the graph. 
+
+        :param sections: a list of section numbers, starting from 0. Sections are assumed to go counterclockwise from 3:00 [0,1,2...]
+        '''
+        nodes = self.nodes
+        edges = self.edges
+        num_sections = len(sections)
+
+        get_angle = lambda vector: (math.atan2(vector.y, vector.x) + 2*math.pi) % (2*math.pi) #angle between 0 and 2pi
+        get_opposite_section = lambda section: (section+4)%8
+
+        for edge in edges.values():
+
+            #determine angle of edge
+            source_node = nodes[edge.source]
+            target_node = nodes[edge.target]
+            vector = SimpleNamespace()
+            vector.x = target_node.x - source_node.x
+            vector.y = target_node.y - source_node.y
+            a = get_angle(vector)
+            print(a)
+            print(vector)
+            del vector
+
+            #determine feasible sections for a given angle
+            section = round((a / (2*math.pi)) * num_sections)
+            next_section = sections[section+1]
+            prev_section = sections[section-1]
+            edge.source_directions = [prev_section, section, next_section]
+            edge.target_directions = list(map(get_opposite_section, edge.source_directions))
 
 if __name__ == '__main__':
+
+    #test code
     graph = Graph('./graphs/test.input.json')
-    print(graph.nodes)
-    print(graph.edges)
+    graph.calc_sections()
+    for edge in graph.edges.values():
+        print(edge.source_directions)
+        print(edge.target_directions, '\n')
